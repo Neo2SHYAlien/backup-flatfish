541f985266eef2a42455c67bc6105c3478bde5ce
Neo2SHYAlien <neo2shyalien@gmail.com>
2016-12-31 12:27:09 +0200

    first commit

---
+++ system/b2g/defaults/pref/user.js
@@ -0,0 +1,27 @@
+pref('geo.gps.supl_server', "supl.izatcloud.net");
+pref('geo.gps.supl_port', 22024);
+pref('dom.payment.provider.0.name', "firefoxmarket");
+pref('dom.payment.provider.0.description', "marketplace.firefox.com");
+pref('dom.payment.provider.0.uri', "https://marketplace.firefox.com/mozpay/?req=");
+pref('dom.payment.provider.0.type', "mozilla/payments/pay/v1");
+pref('dom.payment.provider.0.requestMethod', "GET");
+pref('b2g.system_manifest_url', "app://system.gaiamobile.org/manifest.webapp");
+pref('b2g.neterror.url', "app://system.gaiamobile.org/net_error.html");
+pref('b2g.system_startup_url', "app://system.gaiamobile.org/index.html");
+pref('network.http.max-connections-per-server', 15);
+pref('dom.mozInputMethod.enabled', true);
+pref('layout.css.scroll-behavior.enabled', true);
+pref('layout.css.sticky.enabled', true);
+pref('intl.uidirection.qps-plocm', "rtl");
+pref('ril.debugging.enabled', false);
+pref('dom.mms.version', 17);
+pref('b2g.wifi.allow_unsafe_wpa_eap', true);
+pref('network.dns.localDomains', "gaiamobile.org,theme.gaiamobile.org,privacy-panel.gaiamobile.org,video.gaiamobile.org,ftu.gaiamobile.org,sharedtest.gaiamobile.org,download.gaiamobile.org,collection.gaiamobile.org,email.gaiamobile.org,calendar.gaiamobile.org,camera.gaiamobile.org,keyboard.gaiamobile.org,music.gaiamobile.org,sms.gaiamobile.org,network-alerts.gaiamobile.org,emergency-call.gaiamobile.org,default_theme.gaiamobile.org,findmydevice.gaiamobile.org,settings.gaiamobile.org,bluetooth.gaiamobile.org,gallery.gaiamobile.org,search.gaiamobile.org,wallpaper.gaiamobile.org,wappush.gaiamobile.org,ringtones.gaiamobile.org,communications.gaiamobile.org,operatorvariant.gaiamobile.org,verticalhome.gaiamobile.org,pdfjs.gaiamobile.org,homescreen.gaiamobile.org,costcontrol.gaiamobile.org,clock.gaiamobile.org,bookmark.gaiamobile.org,callscreen.gaiamobile.org,marketplace.firefox.com.gaiamobile.org,fl.gaiamobile.org,system.gaiamobile.org,fm.gaiamobile.org,ds-test.gaiamobile.org,uitest.gaiamobile.org,test-otasp.gaiamobile.org,crystalskull.gaiamobile.org,test-agent.gaiamobile.org,image-uploader.gaiamobile.org,test-receiver-inline.gaiamobile.org,marketplace.allizom.org.gaiamobile.org,test-wappush.gaiamobile.org,marketplace-dev.allizom.org.gaiamobile.org,demo-hci-event.gaiamobile.org,contacts-ds-provider1.gaiamobile.org,custom-origin.gaiamobile.org,membuster.gaiamobile.org,theme-test-1.gaiamobile.org,demo-keyboard.gaiamobile.org,bookmarks-reader.gaiamobile.org,test-container.gaiamobile.org,feedback.gaiamobile.org,test-keyboard-app.gaiamobile.org,geoloc.gaiamobile.org,theme-test-2.gaiamobile.org,test-shared-css.gaiamobile.org,test-l10n-optimize.gaiamobile.org,homescreen-stingray.gaiamobile.org,sheet-app-2.gaiamobile.org,test-iac-subscriber.gaiamobile.org,contacts-manager.gaiamobile.org,share-receiver.gaiamobile.org,test-l10n-missing.gaiamobile.org,test-sensors.gaiamobile.org,test-receiver-2.gaiamobile.org,template.gaiamobile.org,test-ime.gaiamobile.org,sheet-app-3.gaiamobile.org,sheet-app-1.gaiamobile.org,testpermission.gaiamobile.org,in_app_pay_test.gaiamobile.org,theme-test-3.gaiamobile.org,contacts-ds-provider2.gaiamobile.org,test-iac-publisher.gaiamobile.org,cubevid.gaiamobile.org,uitest-privileged.gaiamobile.org,test-fxa-client.gaiamobile.org,music2.gaiamobile.org,test-receiver-1.gaiamobile.org,mochitest.gaiamobile.org,test-findmydevice.gaiamobile.org,dlna-player.gaiamobile.org,smart-system.gaiamobile.org,smart-home.gaiamobile.org,smart-settings.gaiamobile.org,app-deck.gaiamobile.org,tv_shared.gaiamobile.org,tv_build.gaiamobile.org,browser.gaiamobile.org");
+pref('devtools.debugger.prompt-connection', false);
+pref('devtools.debugger.forbid-certified-apps', false);
+pref('javascript.options.discardSystemSource', false);
+pref('b2g.adb.timeout', 0);
+pref('devtools.responsiveUI.customWidth', 1312);
+pref('devtools.responsiveUI.customHeight', 861);
+pref('devtools.responsiveUI.currentPreset', "custom");
+
